057b34450a2a77e592e92cef2d72c7929fb38d59
Nitesh114 <nitesh.dps@gmail.com>
2019-08-10 11:59:06 +0530

    added sample

---
+++ sample.s
@@ -0,0 +1,14 @@
+/******************************************************************************
+* @file sample.s
+* @brief Simple sample program for ARM SIM
+* @author G S Nitesh Narayana
+******************************************************************************/
+
+.global main
+.func main
+
+main:
+
+    MOV r0, #2 
+    MOV r1, #3
+    ADD r3, r2, r1
