3ebcd0b5c81d6162d502acc4cd6cc918d09e2eb6
Nitesh8998 <nitesh.dps@gmail.com>
2019-08-11 22:49:08 +0530

    added sample with updates

--- sample.s
+++ sample.s
@@ -4,6 +4,14 @@
 * @author G S Nitesh Narayana
 ******************************************************************************/
 
+
+/*
+	NOTE: If you are using this file to run it on visUAL,
+	      delete the .global main .function main lines
+	      this is one of the limitations of this tool.
+	      We shall discuss this further in next labs.
+*/
+
 .global main
 .func main
 
@@ -11,4 +19,13 @@ main:
 
     MOV r0, #2 
     MOV r1, #3
-    ADD r3, r2, r1
+    MOV r2, #5
+    MOV r5, #1000
+    MOV r13, #100  @you should not be using r13 r14 r15 change the register here
+
+/*
+	Similarly you can mov values to other registers and work with them.
+	Be careful while loading, the address you specify might not be correct.
+	we shall discuss about addresses later.
+*/
+    
